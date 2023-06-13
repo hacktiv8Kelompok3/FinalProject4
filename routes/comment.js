836de48df4e2627509e0c897052a4cb6793ad665
MuhammadFadhil1024 <61375944+MuhammadFadhil1024@users.noreply.github.com>
2023-06-14 02:58:24 +0700

    add testing for comment

--- routes/comment.js
+++ routes/comment.js
@@ -1,13 +1,12 @@
-const router = require("express").Router()
-const comment = require("../controllers/commentcontroller")
-const authentication = require("../middleware/authentication")
-const authorizationComment = require('../middleware/authorizationComment')
+const router = require("express").Router();
+const comment = require("../controllers/commentcontroller");
+const authentication = require("../middleware/authentication");
+const authorizationComment = require("../middleware/authorizationComment");
 
-router.get("/", comment.getAllComments)
-router.use(authentication)
-router.post("/", comment.postComment)
-router.put("/:commentid", authorizationComment, comment.updateComment)
-router.delete("/:commentid", authorizationComment, comment.deleteComment)
+router.use(authentication);
+router.get("/", comment.getAllComments);
+router.post("/", comment.postComment);
+router.put("/:commentid", authorizationComment, comment.updateComment);
+router.delete("/:commentid", authorizationComment, comment.deleteComment);
 
-
-module.exports = router
+module.exports = router;
